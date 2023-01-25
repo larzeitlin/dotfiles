9d207cd26b2ca656f274cd1614453350f0293b01
Luke Zeitlin <luke.zeitlin@griffin.sh>
2023-01-25 18:06:20 -0300

    Update init.el

--- init.el
+++ init.el
@@ -1,145 +1,240 @@
-
-
-;;;;;;;;;;;;;;;;;;;;; REQUIRE ;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-(defvar my-packages '(doom-themes
-                      clojure-mode
-                      doom-modeline
-                      evil
-                      clj-refactor
-                      ac-cider
-                      flycheck-clj-kondo
-                      auto-complete
-                      cider))
-
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (print (format "Installing %s" p))
-    (package-install p)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq inhibit-startup-message t) ;; No splash screen
-(setq initial-scratch-message nil) ;; No scratch message
-
-
-(defvar user-temporary-file-directory "~/.emacs-backup")
-(make-directory user-temporary-file-directory t)
-(setq backup-by-copying t) 
-(setq backup-directory-alist 
-      `(("." . ,user-temporary-file-directory)
-	(,tramp-file-name-regexp nil)))
-(setq auto-save-list-file-prefix
-      (concat user-temporary-file-directory ".auto-saves-"))
-(setq auto-save-file-name-transforms
-      `((".*" ,user-temporary-file-directory t)))
-
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "615123f602c56139c8170c153208406bf467804785007cdc11ba73d18c3a248b" default)))
- '(initial-buffer-choice t)
- '(minibuffer-prompt-properties
-   (quote
-    (read-only t cursor-intangible t face minibuffer-prompt)))
- '(package-selected-packages
-   (quote
-    (rainbow-delimiters clojure-mode-extra-font-locking paredit-menu helm flycheck-clj-kondo evil doom-themes doom-modeline company clj-refactor aggressive-indent ac-cider))))
-
-;;;;;;;;;;;;;;;;; DOOM THEME ;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq doom-themes-enable-bold nil    ; if nil, bold is universally disabled
-      doom-themes-enable-italic nil) ; if nil, italics is universally disabled
-(doom-themes-visual-bell-config)
-(doom-themes-org-config)
-(load-theme 'doom-spacegrey)
-(doom-modeline-mode 1)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;;;;;;;;;;;;;;;;;; GLOBAL MODES ;;;;;;;;;;;;;;;;;;;;;
-(global-flycheck-mode)
-(global-company-mode)
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-;;;;;;;;;;;;;;;;;;;;;; UI ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (tool-bar-mode -1)
-(setq mode-line-format nil)
-(global-hl-line-mode +1)
-(global-visual-line-mode 1)
-(setq display-line-numbers 'relative)
-(set-face-attribute 'mode-line nil :box nil)
-(set-face-attribute 'mode-line-inactive nil :box nil)
-(setq show-paren-style 'expression)
-(customize-set-variable
- 'minibuffer-prompt-properties
- (quote (read-only t cursor-intangible t face minibuffer-prompt)))
-(setq custom-safe-themes t)
-;;;;;;;;;;;;;;;;;;;;;; HELM ;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x C-b") #'helm-buffers-list)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(global-visual-line-mode t)
+(set-face-attribute 'default nil :height 120)
+(package-initialize)
+(show-paren-mode)
+(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
+(setq inhibit-startup-screen t)
 
-;;;;;;;;;;;;;;;;;;;;;; PARENS ;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'clojure-repl-mode-hook 'paredit-mode)
-(add-hook 'clojure-repl-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'clojurescript-mode-hook 'paredit-mode)
-(add-hook 'clojurescript-mode-hook 'rainbow-delimiters-mode)
+(use-package general
+  :ensure t
+  :init (general-auto-unbind-keys))
 
-;;;;;;;;;;;;;;;;;; EDIT SETTINGS ;;;;;;;;;;;;;;;;;;;;;;
-(global-aggressive-indent-mode 1)
-(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
-(global-display-line-numbers-mode)
-(show-paren-mode 1)
-(evil-mode 1)
-(define-key evil-normal-state-map (kbd "M-.") nil)
+(use-package evil
+  :ensure t
+  :config (evil-mode 1))
 
-;;;;;;;;;;;;;;;;;;;;;; CIDER ;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq cider-repl-pop-to-buffer-on-connect nil)
-(setq cider-popup-stacktraces nil)
+(use-package magit
+  :ensure t
+  :init (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1))
 
-;;;;;;;;;;;;;;;;;; AUTOCOMPLETE ;;;;;;;;;;;;;;;;;;;;;;;
-(ac-config-default)
-(add-to-list 'ac-modes 'cider-mode)
-(add-to-list 'ac-modes 'cider-repl-mode)
-(defun set-auto-complete-as-completion-at-point-function ()
-  (setq completion-at-point-functions '(auto-complete)))
-(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq completion-styles '(flex))
 
-(setq cider-cljs-lein-repl
-      "(do (require 'figwheel-sidecar.repl-api)
-           (figwheel-sidecar.repl-api/start-figwheel!)
-           (figwheel-sidecar.repl-api/cljs-repl))")
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-(setq cider-show-error-buffer nil)
-(setq cider-save-file-on-load t)
+(use-package company
+  :ensure t
+  :custom (add-hook 'after-init-hook 'global-company-mode))
 
-(require 'expand-region)
-(global-set-key (kbd "<M-s-up>") 'er/expand-region)
-(global-set-key (kbd "<M-s-down>") 'er/contract-region)
-(global-set-key (kbd "<M-s-right>") 'paredit-forward)
-(global-set-key (kbd "<M-s-left>") 'paredit-backward)
-(global-set-key (kbd "M-s-c") 'lsp-format-region)
-(global-set-key (kbd "M-s-r") 'paredit-raise-sexp)
-(global-set-key (kbd "M-s-b") 'paredit-wrap-round)
-(global-set-key (kbd "M-s-[") 'paredit-wrap-square)
-(global-set-key (kbd "M-s-{") 'paredit-wrap-curly)
-(global-set-key (kbd "M-s-k") 'paredit-kill)
+(use-package vertico
+  :ensure t
+  :init (vertico-mode)
+  :custom
+  (vertico-cycle t)
+  (setq completion-styles '(hotfuzz flex)))
+
+(use-package savehist
+  :ensure t
+  :init
+  (savehist-mode))
+
+(use-package marginalia
+  :after vertico
+  :ensure t
+  :custom
+  (marginalia-annotators)
+  '(marginalia-annotators-heavy
+    marginalia-annotators-light
+    nil)
+  :init
+  (marginalia-mode))
+
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap ("C-c p" . projectile-command-map))
+
+(use-package auto-package-update
+  :ensure t
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
+(use-package cider
+  :ensure t
+  :init
+  (setq cider-repl-pop-to-buffer-on-connect nil)
+  (setq cider-popup-stacktraces nil)
+  (setq cider-font-lock-dynamically '(macro core function var))
+  (setq cider-show-error-buffer nil)
+  (setq cider-save-file-on-load t))
+
+(use-package consult
+  :ensure t
+  :bind (("C-x b" . consult-buffer)
+	 ("M-y" . consult-yank-pop))
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+  :init 
+  
+  (setq register-preview-delay 0.5
+	register-preview-function #'consult-register-format)
+
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  (setq xref-show-xrefs-function #'consult-xref
+	xref-show-definitions-function #'consult-xref))
+
+(use-package paredit
+  :ensure t 
+  :hook ((cider-repl-mode       . paredit-mode)
+	 (cider-mode            . paredit-mode)
+	 (clojure-mode          . paredit-mode)
+	 (emacs-lisp-mode       . paredit-mode)
+	 (lisp-mode             . paredit-mode)
+	 (lisp-interaction-mode . paredit-mode)
+	 (scheme-mode           . paredit-mode)
+	 (slime-mode            . paredit-mode)))
+
+(use-package expand-region
+  :ensure t
+  :bind (("<M-s-up>" . 'er/expand-region)
+	 ("<M-s-down>" . 'er/contract-region)))
+
+(use-package lsp-treemacs
+  :ensure t)
+
+(use-package flycheck-clj-kondo
+  :ensure t)
+
+(use-package js2-mode
+  :ensure t)
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (require 'flycheck-clj-kondo))
+
+(use-package flycheck 
+  :ensure t
+  :hook ((clojure-mode . flycheck-mode)))
+
+(use-package lsp-mode
+  :ensure t
+  :hook ((clojure-mode       . lsp)
+	 (clojurescript-mode . lsp)
+	 (clojurec-mode      . lsp)
+	 (js2-mode           . lsp)
+	 (python-mode        . lsp))
+  :init
+  (setq gc-cons-threshold (* 100 1024 1024)
+	read-process-output-max (* 1024 1024)
+	treemacs-space-between-root-nodes nil
+	company-minimum-prefix-length 1
+	lsp-lens-enable t))
+
+(use-package git-gutter
+  :ensure t
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :ensure t
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+(use-package vterm
+  :ensure t)
+
+(use-package consult-projectile
+  :ensure t)
+
+(general-def
+  :states  '(normal visual motion)
+  :prefix "SPC"
+  "SPC" 'consult-projectile-find-file
+  "<tab>" 'projectile-switch-project
+  "p" 'consult-git-grep
+  "f" 'consult-line
+  "B" 'consult-line-multi
+  "b" 'consult-buffer
+  "d" 'evil-goto-definition
+  "r" 'lsp-find-references
+  "v" 'vterm-other-window
+  "y" 'consult-yank-pop
+  "o" 'other-window
+  "0" 'delete-window
+  "1" 'delete-other-windows
+  "3" 'split-window-horizontally
+  "2" 'split-window-vertically)
+
+(general-def
+  :states '(normal visual motion)
+  :prefix "SPC s"
+  "r" 'paredit-raise-sexp
+  "k" 'paredit-kill)
+
+(general-def
+  :states '(normal visual motion)
+  :prefix "SPC c"
+  "e l" 'cider-eval-last-sexp
+  "e r" 'cider-eval-region
+  "i" 'cider-inspect-last-sexp
+  "n" 'cider-repl-set-ns
+  "l" 'cider-load-buffer
+  "q" 'cider-quit
+  "r" 'cider-switch-to-repl-buffer
+  "b" 'cider-switch-to-last-clojure-buffer
+  "j s" 'cider-jack-in-cljs
+  "j c" 'cider-jack-in-clj)
+
+(general-def
+  :states '(normal visual motion)
+  :prefix "SPC m"
+  "s" 'magit-status)
+
+(general-def
+  "C-+" 'text-scale-increase
+  "C--" 'text-scale-decrease)
+
+(general-def
+  "<M-s-right>" 'paredit-forward
+  "<M-s-left>" 'paredit-backward)
+
+(use-package windresize
+  :ensure t)
+
+(general-def
+  :states '(normal visual motion)
+  :prefix "SPC w"
+  "r" 'windresize)
+
+(use-package slime
+  :ensure t
+  :config (setq inferior-lisp-program "/usr/bin/sbcl"))
+
+(require 'org-tempo)
+
+(load-theme 'modus-vivendi)
